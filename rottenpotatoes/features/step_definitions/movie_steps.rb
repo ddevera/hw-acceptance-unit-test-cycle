0cc98f9177ebabd16ad3032ac5a296902b668438
Derek Devera <ddevera@g.clemson.edu>
2017-10-14 22:16:17 +0000

    Finished Part 1

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -7,15 +7,14 @@ Given /the following movies exist/ do |movies_table|
   end
 end
 
+Then /the director of "(.*)" should be "(.*)"/ do |movie, director|
+    step %Q{I should see "#{movie}"}
+    step %Q{I should see "#{director}"}
+end
+
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
-
-Then /the director of "(.*)" should be "(.*)"/ do |title, director|
-    step %Q{I should see "#{title}"}
-    step %Q{I should see "#{director}"}
-
-
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
